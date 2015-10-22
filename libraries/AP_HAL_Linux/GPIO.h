ArduPilot/ardupilot

AP_HAL_Linux: make board specific GPIO inclusions last
Board specific inclusions should be the last inclusions so that any
eventual dependency may be fulfilled.

libraries/AP_HAL_Linux/GPIO.h
@@ -4,11 +4,6 @@
 #include "AP_HAL_Linux.h"
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
-#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_PXF || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_ERLE || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
-#include "GPIO_BBB.h"
-#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
-#include "GPIO_RPI.h"
-#endif
 
 class Linux::LinuxDigitalSource : public AP_HAL::DigitalSource {
 public:
@@ -21,6 +16,12 @@ private:
     uint8_t _v;
 
 };
+
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_PXF || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_ERLE || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
+#include "GPIO_BBB.h"
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
+#include "GPIO_RPI.h"
+#endif
 
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_LINUX
 #endif // __AP_HAL_LINUX_GPIO_H__

balattar/Copter

libraries/AP_HAL_Linux/GPIO.h
@@ -4,11 +4,6 @@
 #include "AP_HAL_Linux.h"
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
-#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_PXF || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_ERLEBOARD || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
-#include "GPIO_BBB.h"
-#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_RASPILOT
-#include "GPIO_RPI.h"
-#endif
 
 class Linux::DigitalSource : public AP_HAL::DigitalSource {
 public:
@@ -21,6 +16,12 @@ private:
     uint8_t _v;
 
 };
+
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_PXF || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_ERLEBOARD || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
+#include "GPIO_BBB.h"
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_RASPILOT
+#include "GPIO_RPI.h"
+#endif
 
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_LINUX
 #endif // __AP_HAL_LINUX_GPIO_H__