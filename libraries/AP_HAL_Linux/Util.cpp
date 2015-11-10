ArduPilot/ardupilot

AP_HAL_Linux: Add PWM_Sysfs
This class makes easy to use Linux PWM sysfs interface, available since
Linux 3.10.

More information https://www.kernel.org/doc/Documentation/pwm.txt

libraries/AP_HAL_Linux/Util.cpp
@@ -100,4 +100,51 @@ bool LinuxUtil::is_chardev_node(const char *path)
     return S_ISCHR(st.st_mode);
 }
 
+int LinuxUtil::write_file(const char *path, const char *fmt, ...)
+{
+    va_list args;
+
+    FILE *file = fopen(path, "we");
+    if (!file)
+        return -errno;
+
+    va_start(args, fmt);
+
+    errno = 0;
+    int ret = vfprintf(file, fmt, args);
+    int errno_bkp = errno;
+    fflush(file);
+    fclose(file);
+
+    va_end(args);
+
+    if (ret < 1)
+        return -errno_bkp;
+
+    return ret;
+}
+
+int LinuxUtil::read_file(const char *path, const char *fmt, ...)
+{
+    va_list args;
+
+    FILE *file = fopen(path, "re");
+    if (!file)
+        return -errno;
+
+    va_start(args, fmt);
+
+    errno = 0;
+    int ret = vfscanf(file, fmt, args);
+    int errno_bkp = errno;
+    fclose(file);
+
+    va_end(args);
+
+    if (ret < 1)
+        return -errno_bkp;
+
+    return ret;
+}
+
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_LINUX

balattar/Copter

libraries/AP_HAL_Linux/Util.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <fcntl.h>
 
 extern const AP_HAL::HAL& hal;
 
@@ -109,6 +110,54 @@ bool Util::is_chardev_node(const char *path)
 uint32_t Util::available_memory(void)
 {
     return 256*1024;
+}
+
+int Util::write_file(const char *path, const char *fmt, ...)
+{
+    errno = 0;
+
+    int fd = ::open(path, O_WRONLY | O_CLOEXEC);
+    if (fd == -1) {
+        return -errno;
+    }
+
+    va_list args;
+    va_start(args, fmt);
+
+    int ret = ::vdprintf(fd, fmt, args);
+    int errno_bkp = errno;
+    ::close(fd);
+
+    va_end(args);
+
+    if (ret < 1) {
+        return -errno_bkp;
+    }
+
+    return ret;
+}
+
+int Util::read_file(const char *path, const char *fmt, ...)
+{
+    errno = 0;
+
+    FILE *file = ::fopen(path, "re");
+    if (!file)
+        return -errno;
+
+    va_list args;
+    va_start(args, fmt);
+
+    int ret = ::vfscanf(file, fmt, args);
+    int errno_bkp = errno;
+    ::fclose(file);
+
+    va_end(args);
+
+    if (ret < 1)
+        return -errno_bkp;
+
+    return ret;
 }
 
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_LINUX