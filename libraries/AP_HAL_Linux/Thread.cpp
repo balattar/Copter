ArduPilot/ardupilot

AP_HAL_Linux: Thread: allow to override run method

libraries/AP_HAL_Linux/Thread.cpp
@@ -33,10 +33,20 @@ namespace Linux {
 void *Thread::run_trampoline(void *arg)
 {
     Thread *thread = static_cast<Thread *>(arg);
-
-    thread->_task();
+    thread->run();
 
     return nullptr;
+}
+
+bool Thread::run()
+{
+    if (!_task) {
+        return false;
+    }
+
+    _task();
+
+    return true;
 }
 
 bool Thread::start(const char *name, int policy, int prio)

libraries/AP_HAL_Linux/Thread.h
@@ -38,6 +38,13 @@ public:
 
     virtual ~Thread() { }
 
+    /*
+     * Run the task assigned in the constructor. May be overriden in case it's
+     * preferred to use Thread as an interface or when user wants to aggregate
+     * some initialization or teardown for the thread.
+     */
+    virtual bool run();
+
     bool start(const char *name, int policy, int prio);
 
 protected:

balattar/Copter

libraries/AP_HAL_Linux/Thread.cpp
@@ -33,10 +33,20 @@ namespace Linux {
 void *Thread::_run_trampoline(void *arg)
 {
     Thread *thread = static_cast<Thread *>(arg);
-
-    thread->_task();
+    thread->_run();
 
     return nullptr;
+}
+
+bool Thread::_run()
+{
+    if (!_task) {
+        return false;
+    }
+
+    _task();
+
+    return true;
 }
 
 bool Thread::start(const char *name, int policy, int prio)

libraries/AP_HAL_Linux/Thread.h
@@ -43,6 +43,13 @@ public:
 protected:
     static void *_run_trampoline(void *arg);
 
+    /*
+     * Run the task assigned in the constructor. May be overriden in case it's
+     * preferred to use Thread as an interface or when user wants to aggregate
+     * some initialization or teardown for the thread.
+     */
+    virtual bool _run();
+
     task_t _task;
     bool _started;
     pthread_t _ctx;