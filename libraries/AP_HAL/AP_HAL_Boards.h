ArduPilot/ardupilot

AP_HAL: added Navio2 board definitions

libraries/AP_HAL/AP_HAL_Boards.h
@@ -37,6 +37,7 @@
 #define HAL_BOARD_SUBTYPE_LINUX_BH       1010
 #define HAL_BOARD_SUBTYPE_LINUX_QFLIGHT  1011
 #define HAL_BOARD_SUBTYPE_LINUX_PXFMINI  1012
+#define HAL_BOARD_SUBTYPE_LINUX_NAVIO2   1013
 
 /**
    HAL PX4 sub-types, starting at 2000
@@ -267,6 +268,19 @@
  * 240x240 crop rescaled to 64x64 */
 #define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (3.0 / (2.1 * 2.0 * 240 / 64))
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
+#define HAL_BOARD_LOG_DIRECTORY "/var/APM/logs"
+#define HAL_BOARD_TERRAIN_DIRECTORY "/var/APM/terrain"
+#define HAL_INS_DEFAULT HAL_INS_MPU9250
+#define HAL_BARO_DEFAULT HAL_BARO_MS5611
+#define HAL_BARO_MS5611_I2C_BUS 0
+#define HAL_BARO_MS5611_I2C_ADDR 0x77
+#define HAL_COMPASS_DEFAULT HAL_COMPASS_AK8963_MPU9250
+#define HAL_GPIO_A_LED_PIN        16
+#define HAL_GPIO_B_LED_PIN        16
+#define HAL_GPIO_C_LED_PIN        16
+#define HAL_GPIO_LED_ON           LOW
+#define HAL_GPIO_LED_OFF          HIGH
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2
 #define HAL_BOARD_LOG_DIRECTORY "/var/APM/logs"
 #define HAL_BOARD_TERRAIN_DIRECTORY "/var/APM/terrain"
 #define HAL_INS_DEFAULT HAL_INS_MPU9250

balattar/Copter

libraries/AP_HAL/AP_HAL_Boards.h
@@ -35,6 +35,7 @@
 #define HAL_BOARD_SUBTYPE_LINUX_BH       1010
 #define HAL_BOARD_SUBTYPE_LINUX_QFLIGHT  1011
 #define HAL_BOARD_SUBTYPE_LINUX_PXFMINI  1012
+#define HAL_BOARD_SUBTYPE_LINUX_NAVIO2   1013
 
 /**
    HAL PX4 sub-types, starting at 2000
@@ -271,6 +272,16 @@
  * 240x240 crop rescaled to 64x64 */
 #define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (3.0 / (2.1 * 2.0 * 240 / 64))
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
+#define HAL_BOARD_LOG_DIRECTORY "/var/APM/logs"
+#define HAL_BOARD_TERRAIN_DIRECTORY "/var/APM/terrain"
+#define HAL_INS_DEFAULT HAL_INS_MPU9250_SPI
+#define HAL_INS_MPU9250_NAME "mpu9250"
+#define HAL_BARO_DEFAULT HAL_BARO_MS5611_I2C
+#define HAL_BARO_MS5611_I2C_BUS 1
+#define HAL_BARO_MS5611_I2C_ADDR 0x77
+#define HAL_BARO_MS5611_USE_TIMER false
+#define HAL_COMPASS_DEFAULT HAL_COMPASS_AK8963_MPU9250
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2
 #define HAL_BOARD_LOG_DIRECTORY "/var/APM/logs"
 #define HAL_BOARD_TERRAIN_DIRECTORY "/var/APM/terrain"
 #define HAL_INS_DEFAULT HAL_INS_MPU9250_SPI