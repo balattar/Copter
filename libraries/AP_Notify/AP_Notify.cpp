ArduPilot/ardupilot

AP_Notify: Add BeagleBone Blue

libraries/AP_Notify/AP_Notify.cpp
@@ -110,6 +110,9 @@ struct AP_Notify::notify_events_type AP_Notify::events;
         Buzzer buzzer;
         Display_SSD1306_I2C display;
         NotifyDevice *AP_Notify::_devices[] = {&display, &buzzer};
+    #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBLUE
+        AP_BoardLED boardled;
+        NotifyDevice *AP_Notify::_devices[] = {&boardled};
     #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_RASPILOT
         ToshibaLED_I2C toshibaled;
         ToneAlarm_Linux tonealarm;

balattar/Copter

libraries/AP_Notify/AP_Notify.cpp
@@ -124,6 +124,9 @@ struct AP_Notify::notify_events_type AP_Notify::events;
         Buzzer buzzer;
         Display display;
         NotifyDevice *AP_Notify::_devices[] = {&display, &buzzer};
+    #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BLUE
+        AP_BoardLED boardled;
+        NotifyDevice *AP_Notify::_devices[] = {&boardled};
     #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_RASPILOT
         ToshibaLED_I2C toshibaled;
         ToneAlarm_Linux tonealarm;