ArduPilot/ardupilot

ardupilotwaf: Add BeagleBone Blue

Tools/ardupilotwaf/boards.py
@@ -305,6 +305,16 @@ class bbbmini(linux):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BBBMINI',
         )
 
+class bbblue(linux):
+    toolchain = 'arm-linux-gnueabihf'
+
+    def configure_env(self, cfg, env):
+        super(bbblue, self).configure_env(cfg, env)
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BBBLUE',
+        )
+
 class pxf(linux):
     toolchain = 'arm-linux-gnueabihf'
 

balattar/Copter

Tools/ardupilotwaf/boards.py
@@ -305,6 +305,16 @@ class bbbmini(linux):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BBBMINI',
         )
 
+class blue(linux):
+    toolchain = 'arm-linux-gnueabihf'
+
+    def configure_env(self, cfg, env):
+        super(blue, self).configure_env(cfg, env)
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BLUE',
+        )
+
 class pxf(linux):
     toolchain = 'arm-linux-gnueabihf'
 