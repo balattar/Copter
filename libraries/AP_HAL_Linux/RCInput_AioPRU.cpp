ArduPilot/ardupilot

AP_HAL_Linux: Add BeagleBone Blue to RCInput_AioPRU

libraries/AP_HAL_Linux/RCInput_AioPRU.cpp
@@ -12,7 +12,8 @@
 
 #include <AP_HAL/AP_HAL.h>
 
-#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI || \
+    CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBLUE
 
 #include <errno.h>
 #include <fcntl.h>

balattar/Copter

libraries/AP_HAL_Linux/RCInput_AioPRU.cpp
@@ -12,7 +12,8 @@
 
 #include <AP_HAL/AP_HAL.h>
 
-#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI || \
+    CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BLUE
 
 #include <errno.h>
 #include <fcntl.h>