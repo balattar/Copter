lucasdemarchi/ardupilot

AP_InertialSensor: use MPU6500 on aerofc

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -742,6 +742,10 @@ AP_InertialSensor::detect_backends(void)
         _add_backend(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU9250_NAME), ROTATION_YAW_270));
         break;
 
+    case AP_BoardConfig::PX4_BOARD_AEROFC:
+        _add_backend(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU6500_NAME), ROTATION_ROLL_180_YAW_90));
+        break;
+
     default:
         break;
     }

balattar/Copter

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -742,6 +742,10 @@ AP_InertialSensor::detect_backends(void)
         _add_backend(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU9250_NAME), ROTATION_YAW_270));
         break;
 
+    case AP_BoardConfig::PX4_BOARD_AEROFC:
+        _add_backend(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU6500_NAME), ROTATION_YAW_270));
+        break;
+
     default:
         break;
     }