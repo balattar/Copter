ArduPilot/ardupilot

AP_RangeFinder: add sensor type to backend

libraries/AP_RangeFinder/AP_RangeFinder_MAVLink.cpp
@@ -26,7 +26,7 @@ extern const AP_HAL::HAL& hal;
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_MAVLink::AP_RangeFinder_MAVLink(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_LASER)
 {
     last_update_ms = AP_HAL::millis();
     distance_cm = 0;

libraries/AP_RangeFinder/AP_RangeFinder_PX4_PWM.cpp
@@ -44,7 +44,7 @@ extern "C" {
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_PX4_PWM::AP_RangeFinder_PX4_PWM(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-	AP_RangeFinder_Backend(_ranger, instance, _state),
+	AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_LASER),
     _last_timestamp(0),
     _last_pulse_time_ms(0),
     _disable_time_ms(0),

libraries/AP_RangeFinder/AP_RangeFinder_analog.cpp
@@ -32,7 +32,7 @@ extern const AP_HAL::HAL& hal;
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_analog::AP_RangeFinder_analog(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_LASER)
 {
     source = hal.analogin->channel(ranger._pin[instance]);
     if (source == nullptr) {

libraries/AP_RangeFinder/AP_RangeFinder_uLanding.cpp
@@ -28,7 +28,7 @@ extern const AP_HAL::HAL& hal;
 AP_RangeFinder_uLanding::AP_RangeFinder_uLanding(RangeFinder &_ranger, uint8_t instance,
                                                              RangeFinder::RangeFinder_State &_state,
                                                              AP_SerialManager &serial_manager) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_LASER)
 {
     uart = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Aerotenna_uLanding, 0);
     if (uart != nullptr) {

balattar/Copter

libraries/AP_RangeFinder/AP_RangeFinder_MAVLink.cpp
@@ -26,7 +26,7 @@ extern const AP_HAL::HAL& hal;
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_MAVLink::AP_RangeFinder_MAVLink(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_UNKNOWN)
 {
     last_update_ms = AP_HAL::millis();
     distance_cm = 0;

libraries/AP_RangeFinder/AP_RangeFinder_PX4_PWM.cpp
@@ -44,7 +44,7 @@ extern "C" {
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_PX4_PWM::AP_RangeFinder_PX4_PWM(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-	AP_RangeFinder_Backend(_ranger, instance, _state),
+	AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_UNKNOWN),
     _last_timestamp(0),
     _last_pulse_time_ms(0),
     _disable_time_ms(0),

libraries/AP_RangeFinder/AP_RangeFinder_analog.cpp
@@ -32,7 +32,7 @@ extern const AP_HAL::HAL& hal;
    already know that we should setup the rangefinder
 */
 AP_RangeFinder_analog::AP_RangeFinder_analog(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_UNKNOWN)
 {
     source = hal.analogin->channel(ranger._pin[instance]);
     if (source == nullptr) {

libraries/AP_RangeFinder/AP_RangeFinder_uLanding.cpp
@@ -28,7 +28,7 @@ extern const AP_HAL::HAL& hal;
 AP_RangeFinder_uLanding::AP_RangeFinder_uLanding(RangeFinder &_ranger, uint8_t instance,
                                                              RangeFinder::RangeFinder_State &_state,
                                                              AP_SerialManager &serial_manager) :
-    AP_RangeFinder_Backend(_ranger, instance, _state)
+    AP_RangeFinder_Backend(_ranger, instance, _state, MAV_DISTANCE_SENSOR_RADAR)
 {
     uart = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Aerotenna_uLanding, 0);
     if (uart != nullptr) {