SkyRocketToys/ardupilot

AP_Compass: MMC3416 magnetometer driver

libraries/AP_Compass/AP_Compass_Backend.h
@@ -56,6 +56,7 @@ public:
         DEVTYPE_LIS3MDL = 0x08,
         DEVTYPE_AK09916 = 0x09,
         DEVTYPE_IST8310 = 0x0A,
+        DEVTYPE_MMC3416 = 0x0B,
     };
     
     

balattar/Copter

libraries/AP_Compass/AP_Compass_Backend.h
@@ -60,6 +60,7 @@ public:
         DEVTYPE_AK09916 = 0x09,
         DEVTYPE_IST8310 = 0x0A,
         DEVTYPE_ICM20948 = 0x0B,
+        DEVTYPE_MMC3416 = 0x0C,
     };
 
 