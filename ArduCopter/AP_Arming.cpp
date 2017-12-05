ArduPilot/ardupilot

Copter: eliminate mode_requires_gps

ArduCopter/AP_Arming.cpp
@@ -408,7 +408,7 @@ bool AP_Arming_Copter::pre_arm_gps_checks(bool display_failure)
     }
 
     // check if flight mode requires GPS
-    bool gps_required = copter.mode_requires_GPS();
+    bool gps_required = copter.flightmode->requires_GPS();
 
     #if AC_FENCE == ENABLED
     // if circular fence is enabled we need GPS

balattar/Copter

ArduCopter/AP_Arming.cpp
@@ -378,7 +378,7 @@ bool AP_Arming_Copter::gps_checks(bool display_failure)
     }
 
     // check if flight mode requires GPS
-    bool mode_requires_gps = copter.mode_requires_GPS();
+    bool mode_requires_gps = copter.flightmode->requires_GPS();
 
     // check if fence requires GPS
     bool fence_requires_gps = false;