vgade-invn/ardupilot

AP_InertialSensor: merged in Revo driver

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -23,6 +23,7 @@
 #include "AP_InertialSensor_SITL.h"
 #include "AP_InertialSensor_qflight.h"
 #include "AP_InertialSensor_RST.h"
+#include "AP_InertialSensor_Revo.h"
 
 /* Define INS_TIMING_DEBUG to track down scheduling issues with the main loop.
  * Output is on the debug console. */
@@ -730,6 +731,8 @@ AP_InertialSensor::detect_backends(void)
     ADD_BACKEND(AP_InertialSensor_SITL::detect(*this));
 #elif HAL_INS_DEFAULT == HAL_INS_HIL
     ADD_BACKEND(AP_InertialSensor_HIL::detect(*this));
+#elif CONFIG_HAL_BOARD == HAL_BOARD_REVOMINI
+    ADD_BACKEND(AP_InertialSensor_Revo::probe(*this, hal.spi->get_device(HAL_INS_MPU60x0_NAME), HAL_INS_DEFAULT_ROTATION));
 #elif HAL_INS_DEFAULT == HAL_INS_MPU60XX_SPI && defined(HAL_INS_DEFAULT_ROTATION)
     ADD_BACKEND(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU60x0_NAME),
                                                   HAL_INS_DEFAULT_ROTATION));

libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -28,6 +28,13 @@
 
 #include "AP_InertialSensor.h"
 
+#if CONFIG_HAL_BOARD == HAL_BOARD_REVOMINI
+#include <AP_HAL_REVOMINI/AP_HAL_REVOMINI.h>
+#include <AP_HAL_REVOMINI/GPIO.h>
+#include <AP_HAL_REVOMINI/Scheduler.h>
+using namespace REVOMINI;
+#endif
+
 class AuxiliaryBus;
 class DataFlash_Class;
 

balattar/Copter

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -23,6 +23,7 @@
 #include "AP_InertialSensor_SITL.h"
 #include "AP_InertialSensor_qflight.h"
 #include "AP_InertialSensor_RST.h"
+#include "AP_InertialSensor_Revo.h"
 
 /* Define INS_TIMING_DEBUG to track down scheduling issues with the main loop.
  * Output is on the debug console. */
@@ -730,6 +731,8 @@ AP_InertialSensor::detect_backends(void)
     ADD_BACKEND(AP_InertialSensor_SITL::detect(*this));
 #elif HAL_INS_DEFAULT == HAL_INS_HIL
     ADD_BACKEND(AP_InertialSensor_HIL::detect(*this));
+#elif CONFIG_HAL_BOARD == HAL_BOARD_F4LIGHT
+    ADD_BACKEND(AP_InertialSensor_Revo::probe(*this, hal.spi->get_device(HAL_INS_MPU60x0_NAME), HAL_INS_DEFAULT_ROTATION));
 #elif HAL_INS_DEFAULT == HAL_INS_MPU60XX_SPI && defined(HAL_INS_DEFAULT_ROTATION)
     ADD_BACKEND(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU60x0_NAME),
                                                   HAL_INS_DEFAULT_ROTATION));

libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -28,6 +28,13 @@
 
 #include "AP_InertialSensor.h"
 
+#if CONFIG_HAL_BOARD == HAL_BOARD_F4LIGHT
+#include <AP_HAL_F4Light/AP_HAL_F4Light.h>
+#include <AP_HAL_F4Light/GPIO.h>
+#include <AP_HAL_F4Light/Scheduler.h>
+using namespace F4Light;
+#endif
+
 class AuxiliaryBus;
 class DataFlash_Class;
 