sidbhp/ardupilot

AP_HAL: introduce a CANProtocol interface and remove HAL CAN thread management

libraries/AP_HAL/CAN.h
@@ -32,7 +32,18 @@
 #define MAX_NUMBER_OF_CAN_INTERFACES    2
 #define MAX_NUMBER_OF_CAN_DRIVERS       2
 
-class AP_UAVCAN;
+/**
+ * Interface that CAN protocols need to implement
+ */
+class AP_HAL::CANProtocol {
+public:
+    /* method called when initializing the CAN interfaces
+     *
+     * if initialization doesn't have errors, protocol class
+     * should create a thread to do send and receive operations
+     */
+    virtual void init(uint8_t driver_num) = 0;
+};
 
 /**
  * Single non-blocking CAN interface.
@@ -115,8 +126,6 @@ public:
     virtual bool is_initialized() = 0;
     virtual void initialized(bool val) = 0;
 
-    virtual AP_UAVCAN *get_UAVCAN(void) = 0;
-    virtual void set_UAVCAN(AP_UAVCAN *uavcan) = 0;
     uavcan::ICanDriver* get_driver() { return _driver; }
 private:
     uavcan::ICanDriver* _driver;

balattar/Copter

libraries/AP_HAL/CAN.h
@@ -32,7 +32,18 @@
 #define MAX_NUMBER_OF_CAN_INTERFACES    2
 #define MAX_NUMBER_OF_CAN_DRIVERS       2
 
-class AP_UAVCAN;
+/**
+ * Interface that CAN protocols need to implement
+ */
+class AP_HAL::CANProtocol {
+public:
+    /* method called when initializing the CAN interfaces
+     *
+     * if initialization doesn't have errors, protocol class
+     * should create a thread to do send and receive operations
+     */
+    virtual void init(uint8_t driver_index) = 0;
+};
 
 /**
  * Single non-blocking CAN interface.
@@ -115,8 +126,6 @@ public:
     virtual bool is_initialized() = 0;
     virtual void initialized(bool val) = 0;
 
-    virtual AP_UAVCAN *get_UAVCAN(void) = 0;
-    virtual void set_UAVCAN(AP_UAVCAN *uavcan) = 0;
     uavcan::ICanDriver* get_driver() { return _driver; }
 private:
     uavcan::ICanDriver* _driver;