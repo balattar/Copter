vgade-invn/ardupilot

SITL: Create gps_hdt_enabled varible for enable/disable HDT GPS sentence

libraries/SITL/SITL.h
@@ -119,6 +119,8 @@ public:
     AP_Int8  pin_mask; // for GPIO emulation
     AP_Float speedup; // simulation speedup
     AP_Int8  odom_enable; // enable visual odomotry data
+
+    AP_Int8 gps_hdt_enabled; // enable the output of a NMEA heading HDT sentence
     
     // wind control
     float wind_speed_active;

balattar/Copter

libraries/SITL/SITL.h
@@ -175,6 +175,7 @@ public:
     AP_Int8  telem_baudlimit_enable; // enable baudrate limiting on links
     AP_Float flow_noise; // optical flow measurement noise (rad/sec)
     AP_Int8  baro_count; // number of simulated baros to create
+    AP_Int8 gps_hdg_enabled; // enable the output of a NMEA heading HDT sentence
 
     // wind control
     enum WindType {