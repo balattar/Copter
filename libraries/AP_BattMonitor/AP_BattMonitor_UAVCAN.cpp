ukmook/ardupilot

AP_BattMonitor: Add support for multiple UAVCAN batteries from a single node

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.cpp
@@ -45,7 +45,7 @@ void AP_BattMonitor_UAVCAN::subscribe_msgs(AP_UAVCAN* ap_uavcan)
     }
 }
 
-AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id)
+AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id)
 {
     if (ap_uavcan == nullptr) {
         return nullptr;
@@ -56,14 +56,15 @@ AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_u
             continue;
         }
         AP_BattMonitor_UAVCAN* driver = (AP_BattMonitor_UAVCAN*)AP::battery().drivers[i];
-        if (driver->_ap_uavcan == ap_uavcan && driver->_node_id == node_id) {
+        if (driver->_ap_uavcan == ap_uavcan && driver->_node_id == node_id && this_is_the_battery_youre_looking_for(i, battery_id)) {
             return driver;
         }
     }
     // find empty uavcan driver
     for (uint8_t i = 0; i < AP::battery()._num_instances; i++) {
         if (AP::battery().drivers[i] != nullptr &&
-            AP::battery().get_type(i) == AP_BattMonitor::Type::UAVCAN_BatteryInfo) {
+            AP::battery().get_type(i) == AP_BattMonitor::Type::UAVCAN_BatteryInfo &&
+            this_is_the_battery_youre_looking_for(i, battery_id)) {
 
             AP_BattMonitor_UAVCAN* batmon = (AP_BattMonitor_UAVCAN*)AP::battery().drivers[i];
             batmon->_ap_uavcan = ap_uavcan;
@@ -106,7 +107,7 @@ void AP_BattMonitor_UAVCAN::handle_battery_info(const BattInfoCb &cb)
 
 void AP_BattMonitor_UAVCAN::handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb)
 {
-    AP_BattMonitor_UAVCAN* driver = get_uavcan_backend(ap_uavcan, node_id);
+    AP_BattMonitor_UAVCAN* driver = get_uavcan_backend(ap_uavcan, node_id, cb.msg->battery_id);
     if (driver == nullptr) {
         return;
     }

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
@@ -29,11 +29,16 @@ public:
     }
 
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
-    static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id);
+    static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id);
     static void handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb);
 
 private:
     void handle_battery_info(const BattInfoCb &cb);
+
+    static bool this_is_the_battery_youre_looking_for(uint8_t instance, uint8_t battery_id) {
+        // when serial number is negative, all batteries are accepted. Else, it must match
+        return (AP::battery().get_serial_number(instance) < 0) || (AP::battery().get_serial_number(instance) == (int32_t)battery_id);
+    }
 
     AP_BattMonitor::BattMonitor_State _interim_state;
     BattMonitor_UAVCAN_Type _type;

balattar/Copter

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.cpp
@@ -45,7 +45,7 @@ void AP_BattMonitor_UAVCAN::subscribe_msgs(AP_UAVCAN* ap_uavcan)
     }
 }
 
-AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id)
+AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id)
 {
     if (ap_uavcan == nullptr) {
         return nullptr;
@@ -56,14 +56,15 @@ AP_BattMonitor_UAVCAN* AP_BattMonitor_UAVCAN::get_uavcan_backend(AP_UAVCAN* ap_u
             continue;
         }
         AP_BattMonitor_UAVCAN* driver = (AP_BattMonitor_UAVCAN*)AP::battery().drivers[i];
-        if (driver->_ap_uavcan == ap_uavcan && driver->_node_id == node_id) {
+        if (driver->_ap_uavcan == ap_uavcan && driver->_node_id == node_id && match_battery_id(i, battery_id)) {
             return driver;
         }
     }
     // find empty uavcan driver
     for (uint8_t i = 0; i < AP::battery()._num_instances; i++) {
         if (AP::battery().drivers[i] != nullptr &&
-            AP::battery().get_type(i) == AP_BattMonitor::Type::UAVCAN_BatteryInfo) {
+            AP::battery().get_type(i) == AP_BattMonitor::Type::UAVCAN_BatteryInfo &&
+            match_battery_id(i, battery_id)) {
 
             AP_BattMonitor_UAVCAN* batmon = (AP_BattMonitor_UAVCAN*)AP::battery().drivers[i];
             batmon->_ap_uavcan = ap_uavcan;
@@ -106,7 +107,7 @@ void AP_BattMonitor_UAVCAN::handle_battery_info(const BattInfoCb &cb)
 
 void AP_BattMonitor_UAVCAN::handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb)
 {
-    AP_BattMonitor_UAVCAN* driver = get_uavcan_backend(ap_uavcan, node_id);
+    AP_BattMonitor_UAVCAN* driver = get_uavcan_backend(ap_uavcan, node_id, cb.msg->battery_id);
     if (driver == nullptr) {
         return;
     }

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
@@ -29,11 +29,16 @@ public:
     }
 
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
-    static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id);
+    static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id);
     static void handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb);
 
 private:
     void handle_battery_info(const BattInfoCb &cb);
+
+    static bool match_battery_id(uint8_t instance, uint8_t battery_id) {
+        // when serial number is negative, all batteries are accepted. Else, it must match
+        return (AP::battery().get_serial_number(instance) < 0) || (AP::battery().get_serial_number(instance) == (int32_t)battery_id);
+    }
 
     AP_BattMonitor::BattMonitor_State _interim_state;
     BattMonitor_UAVCAN_Type _type;