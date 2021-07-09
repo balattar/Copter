vgade-invn/ardupilot

AP_Math: allow for double EKF build

libraries/AP_Math/matrix3.cpp
@@ -23,14 +23,14 @@
 // create a rotation matrix given some euler angles
 // this is based on http://gentlenav.googlecode.com/files/EulerAngles.pdf
 template <typename T>
-void Matrix3<T>::from_euler(float roll, float pitch, float yaw)
+void Matrix3<T>::from_euler(T roll, T pitch, T yaw)
 {
-    const float cp = cosf(pitch);
-    const float sp = sinf(pitch);
-    const float sr = sinf(roll);
-    const float cr = cosf(roll);
-    const float sy = sinf(yaw);
-    const float cy = cosf(yaw);
+    const T cp = cosf(pitch);
+    const T sp = sinf(pitch);
+    const T sr = sinf(roll);
+    const T cr = cosf(roll);
+    const T sy = sinf(yaw);
+    const T cy = cosf(yaw);
 
     a.x = cp * cy;
     a.y = (sr * sp * cy) - (cr * sy);
@@ -46,7 +46,7 @@ void Matrix3<T>::from_euler(float roll, float pitch, float yaw)
 // calculate euler angles from a rotation matrix
 // this is based on http://gentlenav.googlecode.com/files/EulerAngles.pdf
 template <typename T>
-void Matrix3<T>::to_euler(float *roll, float *pitch, float *yaw) const
+void Matrix3<T>::to_euler(T *roll, T *pitch, T *yaw) const
 {
     if (pitch != nullptr) {
         *pitch = -safe_asin(c.x);
@@ -89,14 +89,14 @@ Vector3<T> Matrix3<T>::to_euler312() const
   fill the matrix from Euler angles in radians in 312 convention
 */
 template <typename T>
-void Matrix3<T>::from_euler312(float roll, float pitch, float yaw)
+void Matrix3<T>::from_euler312(T roll, T pitch, T yaw)
 {
-    const float c3 = cosf(pitch);
-    const float s3 = sinf(pitch);
-    const float s2 = sinf(roll);
-    const float c2 = cosf(roll);
-    const float s1 = sinf(yaw);
-    const float c1 = cosf(yaw);
+    const T c3 = cosf(pitch);
+    const T s3 = sinf(pitch);
+    const T s2 = sinf(roll);
+    const T c2 = cosf(roll);
+    const T s1 = sinf(yaw);
+    const T c1 = cosf(yaw);
 
     a.x = c1 * c3 - s1 * s2 * s3;
     b.y = c1 * c2;
@@ -127,7 +127,7 @@ void Matrix3<T>::rotate(const Vector3<T> &g)
 template <typename T>
 void Matrix3<T>::normalize(void)
 {
-    const float error = a * b;
+    const T error = a * b;
     const Vector3<T> t0 = a - (b * (0.5f * error));
     const Vector3<T> t1 = b - (a * (0.5f * error));
     const Vector3<T> t2 = t0 % t1;
@@ -238,15 +238,15 @@ void Matrix3<T>::zero(void)
 // create rotation matrix for rotation about the vector v by angle theta
 // See: http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToMatrix/
 template <typename T>
-void Matrix3<T>::from_axis_angle(const Vector3<T> &v, float theta)
+void Matrix3<T>::from_axis_angle(const Vector3<T> &v, T theta)
 {
-    const float C = cosf(theta);
-    const float S = sinf(theta);
-    const float t = 1.0f - C;
+    const T C = cosf(theta);
+    const T S = sinf(theta);
+    const T t = 1.0f - C;
     const Vector3<T> normv = v.normalized();
-    const float x = normv.x;
-    const float y = normv.y;
-    const float z = normv.z;
+    const T x = normv.x;
+    const T y = normv.y;
+    const T z = normv.z;
 
     a.x = t*x*x + C;
     a.y = t*x*y - z*S;

balattar/Copter

libraries/AP_Math/matrix3.cpp
@@ -23,14 +23,14 @@
 // create a rotation matrix given some euler angles
 // this is based on http://gentlenav.googlecode.com/files/EulerAngles.pdf
 template <typename T>
-void Matrix3<T>::from_euler(float roll, float pitch, float yaw)
+void Matrix3<T>::from_euler(T roll, T pitch, T yaw)
 {
-    const float cp = cosf(pitch);
-    const float sp = sinf(pitch);
-    const float sr = sinf(roll);
-    const float cr = cosf(roll);
-    const float sy = sinf(yaw);
-    const float cy = cosf(yaw);
+    const T cp = cosF(pitch);
+    const T sp = sinF(pitch);
+    const T sr = sinF(roll);
+    const T cr = cosF(roll);
+    const T sy = sinF(yaw);
+    const T cy = cosF(yaw);
 
     a.x = cp * cy;
     a.y = (sr * sp * cy) - (cr * sy);
@@ -46,7 +46,7 @@ void Matrix3<T>::from_euler(float roll, float pitch, float yaw)
 // calculate euler angles from a rotation matrix
 // this is based on http://gentlenav.googlecode.com/files/EulerAngles.pdf
 template <typename T>
-void Matrix3<T>::to_euler(float *roll, float *pitch, float *yaw) const
+void Matrix3<T>::to_euler(T *roll, T *pitch, T *yaw) const
 {
     if (pitch != nullptr) {
         *pitch = -safe_asin(c.x);
@@ -80,7 +80,7 @@ void Matrix3<T>::from_rotation(enum Rotation rotation)
 template <typename T>
 Vector3<T> Matrix3<T>::to_euler312() const
 {
-    return Vector3<T>(asinf(c.y),
+    return Vector3<T>(asinF(c.y),
                       atan2f(-c.x, c.z),
                       atan2f(-a.y, b.y));
 }
@@ -89,14 +89,14 @@ Vector3<T> Matrix3<T>::to_euler312() const
   fill the matrix from Euler angles in radians in 312 convention
 */
 template <typename T>
-void Matrix3<T>::from_euler312(float roll, float pitch, float yaw)
+void Matrix3<T>::from_euler312(T roll, T pitch, T yaw)
 {
-    const float c3 = cosf(pitch);
-    const float s3 = sinf(pitch);
-    const float s2 = sinf(roll);
-    const float c2 = cosf(roll);
-    const float s1 = sinf(yaw);
-    const float c1 = cosf(yaw);
+    const T c3 = cosF(pitch);
+    const T s3 = sinF(pitch);
+    const T s2 = sinF(roll);
+    const T c2 = cosF(roll);
+    const T s1 = sinF(yaw);
+    const T c1 = cosF(yaw);
 
     a.x = c1 * c3 - s1 * s2 * s3;
     b.y = c1 * c2;
@@ -127,7 +127,7 @@ void Matrix3<T>::rotate(const Vector3<T> &g)
 template <typename T>
 void Matrix3<T>::normalize(void)
 {
-    const float error = a * b;
+    const T error = a * b;
     const Vector3<T> t0 = a - (b * (0.5f * error));
     const Vector3<T> t1 = b - (a * (0.5f * error));
     const Vector3<T> t2 = t0 % t1;
@@ -238,15 +238,15 @@ void Matrix3<T>::zero(void)
 // create rotation matrix for rotation about the vector v by angle theta
 // See: http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToMatrix/
 template <typename T>
-void Matrix3<T>::from_axis_angle(const Vector3<T> &v, float theta)
+void Matrix3<T>::from_axis_angle(const Vector3<T> &v, T theta)
 {
-    const float C = cosf(theta);
-    const float S = sinf(theta);
-    const float t = 1.0f - C;
+    const T C = cosF(theta);
+    const T S = sinF(theta);
+    const T t = 1.0f - C;
     const Vector3<T> normv = v.normalized();
-    const float x = normv.x;
-    const float y = normv.y;
-    const float z = normv.z;
+    const T x = normv.x;
+    const T y = normv.y;
+    const T z = normv.z;
 
     a.x = t*x*x + C;
     a.y = t*x*y - z*S;