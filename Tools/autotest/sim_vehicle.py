m4a3/ardupilot

Tools: sim_vehicle: pass on slave comandline CMD

Tools/autotest/sim_vehicle.py
@@ -595,6 +595,8 @@ def start_vehicle(binary, autotest, opts, stuff, loc=None):
         cmd.append("-w")
     cmd.extend(["--model", stuff["model"]])
     cmd.extend(["--speedup", str(opts.speedup)])
+    if opts.slave is not None:
+        cmd.extend(["--slave", str(opts.slave)])
     if opts.sitl_instance_args:
         # this could be a lot better:
         cmd.extend(opts.sitl_instance_args.split(" "))
@@ -957,6 +959,10 @@ group_sim.add_option("-Z", "--swarm",
 group_sim.add_option("--flash-storage",
                      action='store_true',
                      help="enable use of flash storage emulation")
+group_sim.add_option("", "--slave",
+                     type='int',
+                     default=0,
+                     help="Set the numver of JSON slave")
 parser.add_option_group(group_sim)
 
 

balattar/Copter

Tools/autotest/sim_vehicle.py
@@ -644,6 +644,8 @@ def start_vehicle(binary, opts, stuff, spawns=None):
     cmd.extend(["--speedup", str(opts.speedup)])
     if opts.sysid is not None:
         cmd.extend(["--sysid", str(opts.sysid)])
+    if opts.slave is not None:
+        cmd.extend(["--slave", str(opts.slave)])
     if opts.sitl_instance_args:
         # this could be a lot better:
         cmd.extend(opts.sitl_instance_args.split(" "))
@@ -1114,6 +1116,10 @@ group_sim.add_option("--ekf-double",
 group_sim.add_option("--ekf-single",
                      action='store_true',
                      help="use single precision in EKF")
+group_sim.add_option("", "--slave",
+                     type='int',
+                     default=0,
+                     help="Set the number of JSON slave")
 parser.add_option_group(group_sim)
 
 